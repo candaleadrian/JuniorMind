0a7e34f5ad805aec9528636b244a1b4a9640360f
candaleadrian <candale.adrian@gmail.com>
2015-11-17 22:34:33 +0200

    afterHint

--- PavajPtPiata/PavajPtPiata/UnitTest1.cs
+++ PavajPtPiata/PavajPtPiata/UnitTest1.cs
@@ -9,24 +9,23 @@ namespace PavajPtPiata
         [TestMethod]
         public void TestMethod1()
         {
-        //calculatorPavaj(1,1,1) == 1;
-
+        Decimal calculatorPavaj(1,1,1) == 1;
+        
         }
-    }
-}
-class Myclass { 
-int calculatorPavaj(int lungime, int latime,int laturaPavaj)
-    {
-    int nrPavajLungime = lungime / laturaPavaj;
-    if (lungime % laturaPavaj != 0)
-    {
-        nrPavajLungime ++;
-    }
-    int nrPavajLatime = latime / laturaPavaj;
-    if (latime  % laturaPavaj != 0)
-    {
-        nrPavajLatime ++;
-    }
-        return (nrPavajLungime * nrPavajLatime) ;
+        int calculatorPavaj(int lungime, int latime, int laturaPavaj)
+        {
+            int nrPavajLungime = lungime / laturaPavaj;
+            if (lungime % laturaPavaj != 0)
+            {
+                nrPavajLungime++;
+            }
+            int nrPavajLatime = latime / laturaPavaj;
+            if (latime % laturaPavaj != 0)
+            {
+                nrPavajLatime++;
+            }
+            return (nrPavajLungime * nrPavajLatime);
+        }
+
     }
 }
